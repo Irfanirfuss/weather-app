d86f29dfe503ad2655db1abf448026553fe6db33
Irfan <irfanirfuss2000@gmail.com>
2024-06-18 17:32:55 +0530

    change style and comments

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -3,12 +3,12 @@ import { RiMoonFill } from "react-icons/ri";
 import { FaClock } from "react-icons/fa";
 
 import "./index.css";
-import "./index.css";
+
 const apiStatus = {
     progress: "progress",
     failure: "failure",
     success: "success",
-};
+}; // update initial view in state
 
 const keys = "7c5acb3360cbf78c38878f05b828837f";
 const api = "https://api.openweathermap.org/data/2.5/";
@@ -30,11 +30,11 @@ class Main extends Component {
 
     changeMode = () => {
         this.setState((prevState) => ({ mode: !prevState.mode }));
-    };
+    }; // changing theme mode
 
     changeInput = (event) => {
         this.setState({ city: event.target.value });
-    };
+    }; // update city in the stste
 
     searchValue = async () => {
         const { city } = this.state;
@@ -43,11 +43,12 @@ class Main extends Component {
         );
 
         const data = await response.json();
-        console.log(response.ok);
+
+        console.log(response.ok); //  fetch the api and checking the response ,
         if (response.ok === true) {
-            this.setState({ weathers: data, status: apiStatus.success });
+            this.setState({ weathers: data, status: apiStatus.success }); // if successfully fetched the  data , so update  in the state
         } else {
-            this.setState({ status: apiStatus.failure });
+            this.setState({ status: apiStatus.failure }); // else  failure response update in the state
         }
     };
 
@@ -83,13 +84,14 @@ class Main extends Component {
                 </p>
             </div>
         );
-    };
+    }; // Showing Success View
 
     failureView = () => (
         <div>
             <h1 className="failure">City Not Found</h1>
         </div>
-    );
+    ); // showing failure view
+
     LoadingView = () => (
         <ul>
             <li></li>
@@ -99,26 +101,28 @@ class Main extends Component {
             <li></li>
             <li></li>
         </ul>
-    );
+    ); // showing Loading view
+
     data = () => {
         const { status } = this.state;
 
         switch (status) {
             case apiStatus.progress:
-                return this.LoadingView();
+                return this.LoadingView(); // showing inital or loading view
             case apiStatus.success:
-                return this.successData();
+                return this.successData(); // showing success view
 
             case apiStatus.failure:
-                return this.failureView();
+                return this.failureView(); // showing failure view
 
             default:
                 return null;
         }
     };
+
     render() {
         const { mode, dateTime, city } = this.state;
-
+        // get the current date and time
         const day = dateTime.getDate();
         const month = dateTime.getMonth() + 1;
         const year = dateTime.getFullYear();
@@ -132,31 +136,28 @@ class Main extends Component {
                 <header>
                     <h1 className="head">Weather App</h1>
                     <div className="wet-con">
-                        <div>
-                            {" "}
-                            <p>
-                                {" "}
-                                <FaClock className="i" /> {hour} : {minute}
-                            </p>
-                        </div>
-                        <div>
-                            <p>
-                                {" "}
-                                {day}-{month}-{year}
-                            </p>
+                        <div className="date-con">
+                            <div>
+                                <p>
+                                    <FaClock className="i" /> {hour} : {minute}
+                                </p>
+                            </div>
+                            <div>
+                                <p>
+                                    {day}-{month}-{year}
+                                </p>
+                            </div>
                         </div>
                         <div>
                             <button
                                 onClick={this.changeMode}
                                 className="mode-btn">
-                                {" "}
                                 <RiMoonFill fontSize={35} color="#fff" />
                             </button>
-                        </div>{" "}
+                        </div>
                     </div>
                 </header>
                 <div>
-                    {" "}
                     <input
                         onChange={this.changeInput}
                         value={city}
@@ -166,7 +167,7 @@ class Main extends Component {
                         Search
                     </button>
                 </div>
-                <div className="data"> {this.data()}</div>
+                <div className="data"> {this.data()} </div>
             </div>
         );
     }
